9045ec394b76c6542fe6051bd8df73d39593bc2b
nandi691 <nbilagi@gmail.com>
2024-02-18 21:25:05 +0530

    Adding Directory Structures

---
+++ Array/SingleDimenstionArray.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+	cout<<"All the best Nandeesh"<<endl;
+	int A[5];
+	A[0] = 12;
+	A[1] = 15;
+	A[2] = 25;
+	
+	cout<<sizeof(A)<<endl;
+	return 0;
+}
